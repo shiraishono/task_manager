02922ba411243b5d0746d3164950d996633f4aca
shiraishono <157781350+shiraishono@users.noreply.github.com>
2024-05-26 21:48:39 +0900

    ver3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,7 +13,7 @@ class TasksController < ApplicationController
     @task = current_user.tasks.build(task_params)
     #現在ログインしているユーザーに関連付けられた新しいタスクのインスタンスを作成し、task_paramsメソッドから取得したパラメータを使用して初期化します。
     if @task.save #タスクの保存を試み、成功したかどうかをチェックします。
-      redirect_to tasks_path,#タスクが正常に保存された場合、タスク一覧ページにリダイレクトします。
+      redirect_to tasks_path,notice: 'タスクが正常に作成されました。'#タスクが正常に保存された場合、タスク一覧ページにリダイレクトします。
     else #タスクの保存に失敗した場合の処理を開始します。
       @tasks = Task.all #再度全てのタスクを取得し、インスタンス変数@tasksに格納します。これにより、エラーメッセージと共にタスク一覧が表示されます。
       render :index #indexテンプレートをレンダリングし、エラーメッセージを表示します。
