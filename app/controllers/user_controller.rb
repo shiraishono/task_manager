66ff165d975d86446d09a5213a166fdb6f9e748a
shiraishono <157781350+shiraishono@users.noreply.github.com>
2024-06-03 22:45:25 +0900

    ver8

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,9 +1,8 @@
 class UsersController < ApplicationController
-  #Userモデルの定義を開始します。ApplicationRecordを継承することで、UserモデルはActive Recordの機能を利用できます。
-  def show #showアクションの定義を開始します。このアクションは特定のユーザーを表示するために使用されます。
-    @user = User.find(params[:id]) #URLパラメーター（params[:id]）からユーザーIDを取得し、そのIDに対応するユーザーをデータベースから検索します。
-    #検索結果はインスタンス変数@userに格納されます。
-    @nickname = @user.nickname #検索したユーザーのニックネームをインスタンス変数@nicknameに格納します。
-    @tasks = @user.tasks #検索したユーザーが所有するタスクをすべて取得し、インスタンス変数@tasksに格納します。
+  def show
+    @user = User.find(params[:id])
+
+    @nickname = @user.nickname
+    @tasks = @user.tasks
   end
 end
