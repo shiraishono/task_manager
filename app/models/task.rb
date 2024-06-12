ff351b5bb72ee6bb9d2a6fe0eb5c25a90de01bd5
shiraishono <157781350+shiraishono@users.noreply.github.com>
2024-06-13 03:24:59 +0900

    render データベース　リセット

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user
   belongs_to :parent_task, class_name: "Task", optional: true
-  has_many   :sub_tasks, class_name: "Task", foreign_key: 'parent_task_id', dependent: :destroy
+  has_many  :sub_tasks, class_name: "Task", foreign_key: "parent_task_id", dependent: :destroy
 
   validates :text, presence: true, length: { maximum: 12 }
 end
